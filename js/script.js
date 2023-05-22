6de398e596c1e5ed992ea22c9386df06eb0d7ab7
Kacper Kosicki <kosickikacper1@gmail.com>
2023-05-22 20:25:45 +0200

    Add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -5,10 +5,11 @@
   });*/
 
   const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
   
-    /* remove class 'active' from all article links */
+    /* [DONE] remove class 'active' from all article links */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -16,9 +17,13 @@
         activeLink.classList.remove('active');
     }
   
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+
+    clickedElement.classList.add('active');
+
+    console.log('clickedElement:', clickedElement);
   
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.posts .active');
 
