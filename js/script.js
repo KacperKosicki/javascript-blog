028cce9aa3fc4d82c765db7eb189893680075e34
Kacper Kosicki <kosickikacper1@gmail.com>
2023-05-22 20:09:59 +0200

    Remove class 'active' from all articles and links

--- js/script.js
+++ js/script.js
@@ -8,11 +8,23 @@
     console.log('Link was clicked!');
     console.log(event);
   
-    /* remove class 'active' from all article links  */
+    /* remove class 'active' from all article links */
+
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
   
     /* add class 'active' to the clicked link */
   
     /* remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('.posts .active');
+
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
   
     /* get 'href' attribute from the clicked link */
   
