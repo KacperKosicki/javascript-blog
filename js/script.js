1a7c256b17faa8a992dd6db5b702d213e40469e9
Kacper Kosicki <kosickikacper1@gmail.com>
2023-05-24 17:31:53 +0200

    Find the correct article using the selector and add class 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -33,15 +33,19 @@
         activeArticle.classList.remove('active');
     }
   
-    /* [IN PROGRESS] get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
 
     const articleSelector = clickedElement.getAttribute("href");
     console.log(articleSelector);
     
 
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
-    /* add class 'active' to the correct article */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector(articleSelector);
+
+    /* [DONE] add class 'active' to the correct article */
+
+    targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
